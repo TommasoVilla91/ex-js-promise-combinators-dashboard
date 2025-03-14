81032cd9a9ea68fb562636bced3e046a3074f9b9
TommasoVilla91 <tommasovilla@yahoo.it>
2025-03-14 18:43:40 +0100

    feat: bonus 1

--- script.js
+++ script.js
@@ -1,34 +1,50 @@
 async function getDashboardData(query) {
-    const cityResponse = await fetch(`https://boolean-spec-frontend.vercel.app/freetestapi/destinations?search=${query}`);
-    const city = await cityResponse.json();
     
-    const weatherResponse = await fetch(`https://boolean-spec-frontend.vercel.app/freetestapi/weathers?search=${query}`);
-    const weather = await weatherResponse.json();
+    try {
+        const cityResponse = await fetch(`https://boolean-spec-frontend.vercel.app/freetestapi/destinations?search=${query}`);
+        const city = await cityResponse.json();
+        
+        const weatherResponse = await fetch(`https://boolean-spec-frontend.vercel.app/freetestapi/weathers?search=${query}`);
+        const weather = await weatherResponse.json();
+    
+        const airportResponse = await fetch(`https://boolean-spec-frontend.vercel.app/freetestapi/airports?search=${query}`);
+        const airport = await airportResponse.json();
+    
+    
+        const [citta, meteo, aeroporto] = await Promise.allSettled([city, weather, airport]);
+    
+        console.log(citta, meteo, aeroporto);
 
-    const airportResponse = await fetch(`https://boolean-spec-frontend.vercel.app/freetestapi/airports?search=${query}`);
-    const airport = await airportResponse.json();
-
-
-    const [citta, meteo, aeroporto] = await Promise.all([city, weather, airport]);
-
-    // console.log(citta[0], meteo[0], aeroporto[0]);    
-
-    return {
-        "city": citta[0].name,
-        "country": citta[0].country,
-        "temperature": meteo[0].temperature,
-        "weather": meteo[0].weather_description,
-        "airport": aeroporto[0].name
+        // BONUS 1 (fatto guardando la soluzione)
+        return {
+            "city": citta[0]?.name ?? null,
+            "country": citta[0]?.country ?? null,
+            "temperature": meteo[0]?.temperature ?? null,
+            "weather": meteo[0]?.weather_description ?? null,
+            "airport": aeroporto[0]?.name ?? null
+        };
+    } catch(error) {
+        throw new Error(`Non riesco a recuperare i dati! ${error.message}`)
     };
-}
+};
 
 (async () => {
-    const data = await getDashboardData('london');
-
-    console.log('Dasboard data:', data);
-     console.log(
-        `${data.city} is in ${data.country}.\n` +
-        `Today there are ${data.temperature} degrees and the weather is ${data.weather}.\n`+
-        `The main airport is ${data.airport}.\n`
-    );
+    try {
+        const data = await getDashboardData('vienna');
+    
+        console.log('Dasboard data:', data);
+        let frase = ``;
+        if (data.city && data.country) {
+            frase += `${data.city} is in ${data.country}.\n`
+        }
+        if (data.temperature && data.weather) {
+            frase += `Today there are ${data.temperature} degrees and the weather is ${data.weather}.\n`
+        }
+        if(data.airport) {
+            frase += `The main airport is ${data.airport}.\n`
+        }
+        console.log(frase);
+    } catch(error) {
+        console.error(error);
+    };
 })();
